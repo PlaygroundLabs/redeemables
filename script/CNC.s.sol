a2dc2099546f2540f89cc62fe5a746d7f2df41cd
Alex Ledger <alex@playgroundlabs.io>
2023-11-04 09:23:19 -0700

    WIP

--- script/CNC.s.sol
+++ script/CNC.s.sol
@@ -13,8 +13,9 @@ import {ERC721OwnerMintable} from "../src/test/ERC721OwnerMintable.sol";
 // TODO: switched to owner mintable
 
 // import {ERC1155ShipyardRedeemableMintable} from "../src/extensions/ERC1155ShipyardRedeemableMintable.sol";
-// import {ERC721ShipyardRedeemableMintable} from "../src/extensions/ERC721ShipyardRedeemableMintable.sol";
+import {ERC721ShipyardRedeemableMintable} from "../src/extensions/ERC721ShipyardRedeemableMintable.sol";
 
+import {ERC721RedemptionMintable} from "../src/extensions/ERC721RedemptionMintable.sol";
 import {ERC721ShipyardRedeemableOwnerMintable} from "../src/test/ERC721ShipyardRedeemableOwnerMintable.sol";
 import {ERC1155ShipyardRedeemableOwnerMintable} from "../src/test/ERC1155ShipyardRedeemableOwnerMintable.sol";
 
@@ -36,7 +37,7 @@ contract DeployAndConfigure1155Receive is Script, Test {
                 "RSRCS"
             );
 
-        ERC721ShipyardRedeemableOwnerMintable ships = new ERC721ShipyardRedeemableOwnerMintable(
+        ERC721ShipyardRedeemableMintable ships = new ERC721ShipyardRedeemableMintable(
                 "Ships",
                 "SHIPS"
             );
@@ -55,7 +56,7 @@ contract DeployAndConfigure1155Receive is Script, Test {
 
         // Configure the considerations. These are the inputs to the redeem,
         // the things the user should must have in order to recieve the offer items
-        ConsiderationItem[] memory consideration = new ConsiderationItem[](3);
+        ConsiderationItem[] memory consideration = new ConsiderationItem[](1);
         consideration[0] = ConsiderationItem({
             itemType: ItemType.ERC721_WITH_CRITERIA,
             token: address(certificates),
@@ -64,22 +65,22 @@ contract DeployAndConfigure1155Receive is Script, Test {
             endAmount: 1,
             recipient: payable(BURN_ADDRESS)
         });
-        consideration[1] = ConsiderationItem({
-            itemType: ItemType.ERC1155,
-            token: address(resources),
-            identifierOrCriteria: 1,
-            startAmount: 100,
-            endAmount: 100,
-            recipient: payable(cncTreasury)
-        });
-        consideration[2] = ConsiderationItem({
-            itemType: ItemType.ERC1155,
-            token: address(resources),
-            identifierOrCriteria: 2,
-            startAmount: 100,
-            endAmount: 100,
-            recipient: payable(cncTreasury)
-        });
+        // consideration[1] = ConsiderationItem({
+        //     itemType: ItemType.ERC1155,
+        //     token: address(resources),
+        //     identifierOrCriteria: 1,
+        //     startAmount: 100,
+        //     endAmount: 100,
+        //     recipient: payable(cncTreasury)
+        // });
+        // consideration[2] = ConsiderationItem({
+        //     itemType: ItemType.ERC1155,
+        //     token: address(resources),
+        //     identifierOrCriteria: 2,
+        //     startAmount: 100,
+        //     endAmount: 100,
+        //     recipient: payable(cncTreasury)
+        // });
         // TODO: ask Ryan about how to do an ERC20 here
         // consideration[3] = ConsiderationItem({
         //     itemType: ItemType.ERC20_WITH_CRITERIA,
@@ -114,11 +115,34 @@ contract DeployAndConfigure1155Receive is Script, Test {
         params.endTime = uint32(block.timestamp + 1_000_000);
         ships.updateCampaign(1, params, "");
 
-        // Mint some tokens
+        // Mint some tokens for the redeem ingredients
+        certificates.mint(msg.sender, 1, 1); // certificate
+        resources.mint(msg.sender, 1, 100); // metal // TODO: shoudl this start at 0 or 1?
+        resources.mint(msg.sender, 2, 100); // wood
 
-        // function _mint(address to, uint256 id, uint256 amount, bytes memory data) internal virtual {
-        // _mint(address(certificates), msg.sender, 1, 100, ""); // metal
-        ships.mint(msg.sender, 1); // ship
-        // _mint(msg.sender, 2, 100, ""); // wood
+        certificates.setApprovalForAll(address(ships), true);
+        resources.setApprovalForAll(address(ships), true);
+
+        // Call redeem
+        // Let's redeem them!
+        uint256 campaignId = 1;
+        uint256 requirementsIndex = 0;
+        bytes32 redemptionHash;
+        uint256 salt;
+        bytes memory signature;
+        bytes memory data = abi.encode(
+            campaignId,
+            requirementsIndex,
+            redemptionHash,
+            salt,
+            signature
+        );
+        assertEq(receiveToken.ownerOf(1), msg.sender);
+
+        uint256[] memory tokenIds = new uint256[](1);
+        tokenIds[0] = 1;
+        // tokenIds[1] = 1;
+        // tokenIds[2] = 2;
+        // ships.redeem(tokenIds, msg.sender, data);
     }
 }
