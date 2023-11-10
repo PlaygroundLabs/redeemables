b738af2ea8c92dc5dc9f7fa1fdd5e8c7c73c0865
Alex Ledger <alex@playgroundlabs.io>
2023-11-09 17:24:51 -0800

    Try writing tests

---
+++ test/TestLootbox.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {ERC721ShipyardRedeemableMintable} from "../src/extensions/ERC721ShipyardRedeemableMintable.sol";
+import {BaseRedeemablesTest} from "./utils/BaseRedeemablesTest.sol";
+
+contract ERC7498_SimpleRedeem is BaseRedeemablesTest {
+    ERC721ShipyardRedeemableMintable lootboxes;
+
+    function setUp() public virtual override {
+        super.setUp();
+
+        lootboxes = new ERC721ShipyardRedeemableMintable(
+            "Captain & Company - Clockwork Lootbox",
+            "CNC-CLBX"
+        );
+    }
+
+    function testRevert() public {
+        // assertEq(1, 1);
+        // https://book.getfoundry.sh/cheatcodes/expect-revert?highlight=expectRevert
+        vm.expectRevert(bytes("NOT_AUTHORIZED"));
+        revert("NOT_AUTHORIZED");
+    }
+
+    function testMint() public {
+        lootboxes.mint(msg.sender, 1);
+        lootboxes.mint(address(0xABCD), 3);
+
+        assertEq(lootboxes.ownerOf(1), msg.sender);
+        assertEq(lootboxes.ownerOf(3), address(0xABCD));
+    }
+
+    function testMintThenBurn() public {
+        address addr1 = address(0xDCBA);
+        address addr2 = address(0xABCD);
+
+        lootboxes.mint(addr1, 1);
+
+        // this burn should fail but it's not because addr2 isn't approved
+        // to burn addr1's token.
+        vm.startPrank(addr2);
+        // vm.expectRevert("burn fail");
+        lootboxes.burn(1);
+
+        // lootboxes.mint(msg.sender, 2);
+        // assertEq(lootboxes.ownerOf(2), addr2);
+
+        // vm.prank(addr2); // https://book.getfoundry.sh/cheatcodes/prank
+        // lootboxes.burn(2);
+    }
+}
